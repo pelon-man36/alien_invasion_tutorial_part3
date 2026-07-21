37267ccd5376d4168320501d3640f74507b3520f
Johan Ramirez <johand200505@gmail.com>
2026-07-21 14:26:58 -0400

    Pt3 - Setup button to start game and make mouse disappear, made sure no keydown events are captured until button is pressed

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,7 @@ from arsenal import Arsenal
 #from alien import Alien
 from alien_fleet import Alien_Fleet
 from time import sleep
+from button import Button
 
 class AlienInvasion:
     def __init__(self):
@@ -39,7 +40,8 @@ class AlienInvasion:
         self.alien_fleet = Alien_Fleet(self)
         self.alien_fleet.create_fleet()
         
-        self.game_active = True
+        self.play_button = Button(self, "Play")
+        self.game_active = False
 
 
     def run_game(self):
@@ -87,10 +89,25 @@ class AlienInvasion:
         self.alien_fleet.fleet.empty()
         self.alien_fleet.create_fleet()
 
+    def restart_game(self):
+        # Setting up dynamic settings
+        # Reset Game Stats
+        # Update HUD scores
+        self._reset_level()
+        self.ship._center_ship()
+        self.game_active = True
+        pygame.mouse.set_visible(False)
+
     def _update_screen(self):
         self.screen.blit(self.bg, (0,0))
         self.ship.draw()
         self.alien_fleet.draw()
+
+        if not self.game_active:
+            self.play_button.draw()
+            pygame.mouse.set_visible(True)
+
+
         pygame.display.flip()
 
     def _check_events(self):
@@ -99,10 +116,18 @@ class AlienInvasion:
                 self.running = False
                 pygame.quit()
                 sys.exit()
-            elif event.type == pygame.KEYDOWN:
+            elif event.type == pygame.KEYDOWN and self.game_active == True:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                self._check_button_clicked()
+
+    def _check_button_clicked(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.play_button.check_clicked(mouse_pos):
+            self.restart_game()
+
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
